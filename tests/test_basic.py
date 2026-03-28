12f0d177e7256e1834d269e49c551a88fd3bc147
Amit <arb11459@gmail.com>
2026-03-28 12:49:08 +0530

    Added CI pipeline with linting, type checking and tests

---
+++ tests/test_basic.py
@@ -0,0 +1,17 @@
+def test_import_body():
+    import body
+
+def test_import_brain():
+    import brain
+
+def test_import_body_daemon_patch():
+    import body.daemon_patch
+
+def test_import_body_adapters_base():
+    import body.adapters.base
+
+def test_import_body_service():
+    import body.services
+
+def test_brain_nox_body_client():
+    import brain.nox_body_client
